f790c13aed4c9f828cd0f8c6e4ae93870f872140
TTsdzb <ttsdzb@outlook.com>
2023-03-21 11:14:07 +0800

    fix: set default qps to 0.5
    Default restriction to free users is 1 qps, which is smaller than 2.

--- src/index.ts
+++ src/index.ts
@@ -65,7 +65,7 @@ namespace Alapi {
 
   export const Config: Schema<Config> = Schema.object({
     token: Schema.string().description("账户的API Token").required(),
-    qps: Schema.number().description("每秒请求数，用于限制请求速度").default(2),
+    qps: Schema.number().description("每秒请求数，用于限制请求速度").default(0.5),
   });
 }
 
